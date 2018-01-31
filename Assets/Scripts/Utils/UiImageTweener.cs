416ae1297db483a96b4329faa3384f860c845d0d
Alphapolygon <janne.piekkola@gmail.com>
2018-01-31 17:14:31 +0200

    h

---
+++ Assets/Scripts/Utils/UiImageTweener.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiImageTweener : MonoBehaviour {
+
+    [SerializeField]
+    private PathTool wayPoints;
+    [SerializeField]
+    int wayPointCount = 0;
+    private Transform MovetoPos;
+    bool move = false;
+
+    void Start() {
+        GetComponent<RectTransform>().anchoredPosition3D = wayPoints.nodes[0].GetComponent<RectTransform>().anchoredPosition3D;
+        onMoveTo();
+
+    }
+
+    public void onMoveTo() {
+        move = true;
+        wayPointCount++;
+        if (wayPointCount >= wayPoints.nodes.Count) wayPointCount = 0;
+        MovetoPos = wayPoints.nodes[wayPointCount];
+        this.GetComponent<DoTweenRectTransformHelper>().onTweenTo(MovetoPos);
+    }
+}
